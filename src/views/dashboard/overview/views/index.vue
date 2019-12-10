6028d438bf3be95f384aeeb0a43d53d9a9ec42f4
Rainfore <rainforest92@126.com>
2019-12-11 00:16:54 +0800

    :rotating_light: Disable alignAttributesVertically

--- src/views/dashboard/overview/views/index.vue
+++ src/views/dashboard/overview/views/index.vue
@@ -10,14 +10,14 @@
         <u-grid-layout-column :span="6">
             <u-panel title="云主机资源趋势">
                 <u-line-chart :x-axis="{ key: 'week' }" :y-axis="{ min: 0 }" :smooth="true" :series="[{ key: 'number' }]" :data="[{ week: '星期一', number: 150 }, { week: '星期二', number: 300 }, { week: '星期三', number: 28 }, { week: '星期四', number: 200 }, { week: '星期五', number: 74 }, { week: '星期六', number: 532 }, { week: '星期日', number: 420 }]" legend
-                              style="height: 270px">
+                    style="height: 270px">
                 </u-line-chart>
             </u-panel>
         </u-grid-layout-column>
         <u-grid-layout-column :span="6">
             <u-panel title="虚拟资源容量">
                 <u-line-chart :x-axis="{ key: 'week' }" :y-axis="{ min: 0 }" :series="[{ key: 'number' }]" :data="[{ week: '星期一', number: 150 }, { week: '星期二', number: 300 }, { week: '星期三', number: 28 }, { week: '星期四', number: 200 }, { week: '星期五', number: 74 }, { week: '星期六', number: 532 }, { week: '星期日', number: 420 }]" legend
-                              style="height: 270px"
+                    style="height: 270px"
                 ></u-line-chart>
             </u-panel>
         </u-grid-layout-column>
@@ -26,7 +26,7 @@
         <u-grid-layout-column :span="12">
             <u-panel title="监控">
                 <u-line-chart :x-axis="{ key: 'week' }" :y-axis="{ min: 0 }" :series="[{ key: 'number' }]" :data="[{ week: '星期一', number: 150 }, { week: '星期二', number: 300 }, { week: '星期三', number: 28 }, { week: '星期四', number: 200 }, { week: '星期五', number: 74 }, { week: '星期六', number: 532 }, { week: '星期日', number: 420 }]" legend
-                              style="height: 270px"
+                    style="height: 270px"
                 ></u-line-chart>、
             </u-panel>
         </u-grid-layout-column>
