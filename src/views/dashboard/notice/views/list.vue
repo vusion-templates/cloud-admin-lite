be67b96ddf6639ba171e84733e0402c54a804e4c
zhangsanshi <zhangsanshi5566@gmail.com>
2020-03-15 23:38:30 +0800

    :bug: remove totalPage

--- src/views/dashboard/notice/views/list.vue
+++ src/views/dashboard/notice/views/list.vue
@@ -32,7 +32,7 @@
             <u-linear-layout direction="vertical">
                 <u-combo-pagination show-total show-sizer show-jumper
                     :page-size-options="limitList" :total-items="total" :page-size.sync="limit"
-                    :total="totalPage" :page="page" @change="changePage($event)" @change-page-size="changeLimit">
+                    :page="page" @change="changePage($event)" @change-page-size="changeLimit">
                 </u-combo-pagination>
             </u-linear-layout>
         </div>
