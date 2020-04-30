bed642304c513d399107f617337a101515c72f97
zhangsanshi <zhangsanshi5566@gmail.com>
2020-04-30 15:45:50 +0800

    remove unused

--- src/global/components/common/index.js
+++
@@ -1,7 +0,0 @@
-export { default as UPageSummary } from './u-page-summary.vue';
-export { default as USearch } from './u-search.vue';
-export { default as UHeadCard } from './u-head-card.vue';
-export { default as SCrumb } from './s-crumb.vue';
-export { default as SSidebar } from './s-sidebar.vue';
-export { default as SNavbar } from './s-navbar.vue';
-export { default as SLogo } from './s-logo.vue';
