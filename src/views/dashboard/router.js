af17f7388d644f35bb819333b1141687afb47bd2
zhangsanshi <zhangsanshi5566@gmail.com>
2020-05-11 14:06:00 +0800

    fix: router instance

--- src/views/dashboard/router.js
+++ src/views/dashboard/router.js
@@ -4,8 +4,5 @@ import routes from './routes';
 let routerInstance;
 export default routerInstance;
 export function initRouter(base) {
-    if (routerInstance) {
-        return routerInstance;
-    }
     return (routerInstance = router(routes, (base || process.env.VUE_APP_BASEURL || '/dashboard'), (title) => title + ' - ' + appConfig.title));
 }
