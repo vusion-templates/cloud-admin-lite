eac8f3fcc83146b49b3c0bfda8d3623cf4c4f846
zhangsanshi <zhangsanshi5566@gmail.com>
2020-05-30 12:18:18 +0800

    :bug: nav active for micro router

--- src/global/components/common/s-navbar.vue
+++ src/global/components/common/s-navbar.vue
@@ -5,13 +5,13 @@
             <template v-for="(group, index) in config">
                 <u-navbar-dropdown v-if="group.children" :title="group.title" :key="group.title">
                     <u-navbar-menu style="min-width: 150%">
-                        <u-navbar-menu-item v-for="item in group.children" :key="item.title" :disabled="item.disabled" :to="item.to" :href="item.href" :target="item.href ? '_blank' : '_self'">
+                        <u-navbar-menu-item v-for="item in group.children" :active-rule="item.activeRule" :key="item.title" :disabled="item.disabled" :to="item.to" :href="item.href" :target="item.href ? '_blank' : '_self'">
                             {{ item.title }}
                         </u-navbar-menu-item>
                     </u-navbar-menu>
                 </u-navbar-dropdown>
                 <u-navbar-divider v-else-if="group === '|'" :key="index"></u-navbar-divider>
-                <u-navbar-item v-else :key="group.title" :disabled="group.disabled" :to="group.to" :href="group.href" :target="group.href ? '_blank' : '_self'">{{ group.title }}</u-navbar-item>
+                <u-navbar-item v-else :key="group.title" :disabled="group.disabled" :active-rule="group.activeRule" :to="group.to" :href="group.href" :target="group.href ? '_blank' : '_self'">{{ group.title }}</u-navbar-item>
             </template>
         </template>
         <template #right><slot name="right"></slot></template>
