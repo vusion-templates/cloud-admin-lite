bebd0ce09ab782e2c2ab24321f1ad03dc43d96ce
Forrest <rainforest92@126.com>
2020-06-10 08:33:30 +0800

    :sparkles: Tabs 使用原始组件,可视化好编辑

--- src/views/dashboard/demo/views/list/tabs/index.vue
+++ src/views/dashboard/demo/views/list/tabs/index.vue
@@ -2,31 +2,14 @@
     <div>
         <u-page-summary>这里是 tab 列表，有个 tab 下面有一个不同的页面</u-page-summary>
         <u-tabs router>
-            <u-tab v-for="tab in tabs" :title="tab.title" :to="tab.to" :key="tab.to"></u-tab>
+            <u-tab title="列表页" to="/demo/list/tabs/basic"></u-tab>
+            <u-tab title="本地分页" to="/demo/list/tabs/localList"></u-tab>
+            <u-tab title="列表页(无分页)" to="/demo/list/tabs/noPageList"></u-tab>
         </u-tabs>
         <router-view></router-view>
     </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            tabs: [
-                {
-                    title: '列表页',
-                    to: '/demo/list/tabs/basic',
-                },
-                {
-                    title: '本地分页',
-                    to: '/demo/list/tabs/localList',
-                },
-                {
-                    title: '列表页(无分页)',
-                    to: '/demo/list/tabs/noPageList',
-                },
-
-            ],
-        };
-    },
 };
 </script>
