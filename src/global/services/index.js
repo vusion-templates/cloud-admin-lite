8b7e7677d735eeb22dbaba66a01db50731b4ac35
zhangsanshi <zhangsanshi5566@gmail.com>
2020-06-24 16:56:41 +0800

    feat: support gloabl services

--- src/global/services/index.js
+++ src/global/services/index.js
@@ -2,10 +2,24 @@ const services = {};
 
 function importAll(r) {
     r.keys().forEach((key) => {
-        services[key] = r(key);
+        const serviceFileContent = r(key).default;
+        const moduleServiceName = key.replace('./', '').replace('.js', '').split('/');
+        if (moduleServiceName.length > 1) {
+            const last = moduleServiceName.pop();
+            if (last !== 'index') {
+                moduleServiceName.push(last);
+            }
+        }
+        const namespace = moduleServiceName.reduce((pre, current) => {
+            if (pre) {
+                current = current.replace(/^[a-z]/, (s) => s.toUpperCase()).replace(/-([a-z])/g, (a, s) => s.toUpperCase());
+            }
+            return pre + current;
+        }, '');
+        services[namespace] = serviceFileContent;
     });
 }
 
-importAll(require.context('./', true, /\/services\/(.*?)\/index\.js$/));
+importAll(require.context('./', true, /\/(.*?)\/index\.js$/));
 
 export default services;
